407ccf892e88de6c055330b2a9067e5f2ba7271d
Laura Zhang <71244459+hiyakawa@users.noreply.github.com>
2021-11-26 16:28:34 +0800

    prob1_20211126

--- prob1/generate.py
+++ prob1/generate.py
@@ -1,10 +1,11 @@
 import jinja2
 
-title = 'Hello, world!'
+title = 'hello'
+content = 'Hello, world!'
 output_file = 'hello.html'
 
 subs = jinja2.Environment(
     loader = jinja2.FileSystemLoader('./')
-).get_template('template.html').render(title = title)
+).get_template('template.html').render(title = title, content = content)
 
 with open(output_file, 'w') as f: f.write(subs)
