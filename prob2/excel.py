83407f86511244ec95bb1ca9e68c68e66880eddc
Laura Zhang <71244459+hiyakawa@users.noreply.github.com>
2022-02-14 14:15:15 +0800

    Update excel.py

--- prob2/excel.py
+++ prob2/excel.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, render_template, flash, redirect, url_for, make_response
-from flask_bootstrap import Bootstrap
+from flask import Flask, request, render_template, flash, redirect, url_for, make_response, send_file
 from werkzeug.utils import secure_filename
+from pypinyin import lazy_pinyin
+from uuid import uuid4
 import os
-import xlrd
+import pandas as pd
 
 app = Flask(__name__)
-
-bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = os.urandom(24)
-
 base_dir = os.path.abspath(os.path.dirname(__file__))
 upload_dir = os.path.join(base_dir, 'static/uploads')
+
+def random_filename(string):
+    ident = uuid4().__str__()
+    return f"{ident}-{string}"
 
 @app.route('/select', methods = ['GET', 'POST'])
 def select():
@@ -25,23 +27,17 @@ def print_table():
             os.makedirs(upload_dir)
 
         if f:
-            file_name = secure_filename(f.filename)
+            original_filename = secure_filename(''.join(lazy_pinyin(f.filename)))
             valid_types = ['xls', 'xlsx']
 
-            if file_name.split('.')[-1] in valid_types:
-                upload_path = os.path.join(upload_dir, file_name)
+            if original_filename.split('.')[-1] in valid_types:
+                unique_filename = random_filename(original_filename)
+                upload_path = os.path.join(upload_dir, unique_filename)
                 f.save(upload_path)
                 flash('Successfully uploaded', 'success')
 
-                datalist = xlrd.open_workbook(upload_path, encoding_override="utf-8")
-                table = datalist.sheets()[0]
-                nrows = table.nrows
-                ncols = table.ncols
-
-                titles = [table.cell_value(0, j) for j in range(ncols)]
-                rows = [table.row_values(i) for i in range(1, nrows)]
-
-                return render_template('result.html', titles=titles, rows=rows)
+                df = pd.read_excel(upload_path)
+                return render_template('result.html', titles = df.columns.values, rows = list(df.values.tolist()))
 
             else:
                 flash('Invalid file type', 'danger')
